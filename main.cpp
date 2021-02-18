1d0a7c0e39a142bc0b66c2f162e026da0bf8c417
tylercchase <tylercchase@gmail.com>
2021-02-18 10:05:18 -0900

    Add comment for feature

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 int main(){
     std::cout << "Hello World" << std::endl;
+    // Range base for loop with initializaer
     for(std::vector nums{1,2,3}; auto& e : nums){
         std::cout << e << std::endl;
     }
